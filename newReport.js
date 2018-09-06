257a04255773867c8840f0bc8bc857b7fc89c271
JoonSe Lim <joonluvschipotle@JoonSes-MacBook-Pro-2.local>
2018-09-06 21:00:12 +0900

    time:

---
+++ newReport.js
@@ -0,0 +1,116 @@
+var currentTime = new Date();
+var currentHour = currentTime.getHours();
+var currentMinute = currentTime.getMinutes();
+
+
+var msg = '12시22분 제보 사과 5성';
+
+
+var splitMessage = msg.split(' ');
+var dummySplit = msg.split(' ');
+
+var msgTime = '';
+var msgTime2;
+var msgHour;
+var msgMin;
+var endHour;
+var endMin;
+var raidTime;
+var raidSentence;
+
+var i;
+
+for (i=0; i<splitMessage.length; i++){
+    if (splitMessage[i].includes("제보")){
+        splitMessage.splice(i,1);
+    }
+}
+
+    
+for (i=0; i<splitMessage.length; i++){
+    if (splitMessage[i].includes("시") && (splitMessage[i].slice(-1)!='시')){
+        msgTime = splitMessage[i];
+        var twoCheck = 0;
+        if (msgTime.slice(-1)=='분'){
+            msgTime = msgTime.replace('분','');
+            twoCheck++;
+        }
+        msgTime = msgTime.replace('시',':');
+        if (twoCheck!=0){
+            splitMessage.splice(i,1)
+        } else if(twoCheck==0){
+            splitMessage.splice(i,1);
+        }
+        splitMessage.push(msgTime);
+    } else if (splitMessage[i].includes("시") && !(splitMessage[i].includes("분"))){
+        splitMessage.push(msgTime);
+        msgTime=splitMessage[i]+splitMessage[i+1];
+        msgTime = msgTime.replace('시',':');
+        msgTime = msgTime.replace('분','');
+        splitMessage.splice(i,2);
+        splitMessage.push(msgTime);
+    } else if(splitMessage[i].includes(":")){
+        msgTime=splitMessage[i]
+        splitMessage.splice(i,1);
+        splitMessage.push(msgTime);
+    }
+}
+
+msgTime2 = splitMessage.splice(-1)[0];
+msgHour = msgTime2.slice(0,msgTime.indexOf(':'));
+msgMin = msgTime2.slice(msgTime.indexOf(':')+1,5)
+
+if (msgHour > 12){
+    msgHour = parseInt(msgHour) - 12;
+}
+
+if (msgHour == 12){ // 12시
+    if (msgMin > 14){ // 12시15분 ~ 59분
+        endHour = 1;
+        endMin = parseInt(msgMin)-15;
+        if (endMin<10){
+            endMin = '0' + endMin;
+        }
+    } else if (msgMin < 15){
+        endHour = msgHour;
+        endMin = parseInt(msgMin)+45;
+    }
+} else {
+    if (msgMin > 14){
+        endHour = parseInt(msgHour) + 1;
+        endMin = parseInt(msgMin)-15;
+        if (endMin<10){
+            endMin = '0' + endMin;
+        }
+    } else if (msgMin < 15){
+        endHour = msgHour;
+        endMin = parseInt(msgMin)+45;
+    }
+}
+if (parseInt(msgMin) < 10){
+    msgMin = '0' + parseInt(msgMin);
+}
+
+raidTime = msgHour+':'+msgMin+'~'+endHour+':'+endMin;
+raidSentence = raidTime + splitMessage.splice(0);
+
+
+
+
+console.log(raidSentence);
+
+
+
+    /*
+    
+for (i=0; i<splitMessage.length; i++){
+    if (splitMessage[i].includes(":")){
+        msgTime=splitMessage[i];
+        
+    } else if(splitMessage[i] == '제보'){
+        
+    }
+}
+
+   */
+
