221332ddf7423458694940ef479f4e945a49811e
JoonSe Lim <joonluvschipotle@JoonSes-MacBook-Pro-2.local>
2018-10-14 17:36:23 +0900

    added some

--- old2/bring.js
+++ old2/bring.js
@@ -1,1 +1,12 @@
-export var i = 2;
+var msg = "구구 아아아아잉고ㅗ오오오 이게 뭐야아아아아"
+
+msg = msg.split(" ");
+
+var quoteName = msg[0]; 
+var quoteQuote = "";
+for (var i=1; i < msg.length; i++ ) {
+    quoteQuote = quoteQuote + " " + msg[i];
+}
+quoteQuote = quoteQuote.trim();
+console.log(quoteName);
+console.log(quoteQuote);
